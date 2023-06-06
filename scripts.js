0968f4d4a4425776f835c0639fef852327409fab
Milindcode <milindsingh1004@gmail.com>
2023-06-06 23:29:59 +0530

    Completed grid logic

--- scripts.js
+++ scripts.js
@@ -2,6 +2,9 @@ const grid = document.querySelector(".grid-main");
 const input = document.querySelector("input");
 const apply_btn = document.querySelector("#apply");
 
+
+
+
 apply_btn.addEventListener('click', () =>{
     let gridSize= parseInt(input.value);
     console.log(gridSize);
@@ -30,5 +33,18 @@ const addGrid = (range) => {
     for(let i=0; i<range; i++){
         addRow(range);
     }
+
+
+
+    const cells= document.querySelectorAll('.cell');
+    console.log(cells);
+
+    cells.forEach((cell) =>{
+        cell.addEventListener('mouseenter', () =>{
+            cell.style.backgroundColor = 'black';
+        })
+    })
 }
+
+addGrid(10);
 
